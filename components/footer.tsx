40d540dee2c801ebc3df277a5efe4da1ef60e1a8
OlekAleksander <olek.rygallo@gmail.com>
2024-01-26 22:59:49 +0100

    Fix margin on the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
     return (
         <div className='border-t py-4 absolute mt-12 left-0 right-0'>
             <div className='max-w-screen-xl m-auto text-foreground/90 justify-between flex-grow flex'>
-                <div className='my-auto mr-2'>
+                <div className='my-auto ml-2'>
                     © 2023-2024{' '}
                     <Link
                         className='my-auto text-foreground hover:underline duration-75 hover:text-white'
